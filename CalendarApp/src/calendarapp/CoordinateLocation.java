79a94c0cfcf7bab4aee563b5406246b18fe6dd6d
Michael Kramer <mpk5206@psu.edu>
2017-02-07 23:38:17 -0500

    Created the CoordinateLocation class

---
+++ CalendarApp/src/calendarapp/CoordinateLocation.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calendarapp;
+
+/**
+ *
+ * @author Faust
+ */
+public class CoordinateLocation extends Location {
+
+    private double longitude = 0.00;
+    private double latitude = 0.00;
+    
+    //There should be some type of verification on the numbers before this point
+    public CoordinateLocation(String name, String description, double latitude,
+            double longitude) {
+        super(name, description);
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+
+    
+    //Accessors
+    /**
+     * @return the Location object's longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @return the Location object's latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+    
+    
+    //Mutators
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public String toString() {
+        return ("Latitude: " + this.latitude + ", Longitude: " + this.longitude);
+    }
+    
+}
