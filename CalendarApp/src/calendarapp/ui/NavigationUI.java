c9a4e5bb6be4878493262f73e4c68b7eaed262e4
Michael Kramer <mpk5206@psu.edul>
2017-03-23 17:54:54 -0400

    Stared building the MenuBar

--- CalendarApp/src/calendarapp/ui/NavigationUI.java
+++ CalendarApp/src/calendarapp/ui/NavigationUI.java
@@ -13,10 +13,14 @@ import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
@@ -32,13 +36,7 @@ import javax.swing.JTextField;
  */
 public class NavigationUI extends JFrame {
 	private NavigationController parentController;
-	private JLabel usernameLbl;
-	private JLabel passwordLbl;
-	private JTextField usernameTxtFld;
-	private JPasswordField passwordFld;
-	private JButton submitBtn;
-	private JButton cancelBtn;
-	private JButton registerBtn;
+	private JMenuBar menuBar;
 	
 	public NavigationUI(NavigationController parentController) {
 		System.out.println("Creating NavigationUI.");
@@ -62,6 +60,73 @@ public class NavigationUI extends JFrame {
 	}
 	
 	private void addComponents() {
-		
+		menuBar = new JMenuBar();
+		buildMenuBar();
+		this.setJMenuBar(menuBar);
+	}
+
+	private void buildMenuBar() {
+		buildFileMenu();
+		//buildEditMenu();
+		buildViewMenu();
+
+	}
+
+	private void buildFileMenu() {
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F); //Sets F as shortcut key
+		buildEventMenu(fileMenu);
+		buildContactMenu(fileMenu);
+		menuBar.add(fileMenu);
+	}
+
+	private void buildEventMenu(JMenu fileMenu) {
+		//Build Event Menu
+		JMenu eventMenu = new JMenu("Events");
+		eventMenu.setMnemonic(KeyEvent.VK_E);
+		JMenuItem createEventBtn = new JMenuItem("Create a New Event");
+		eventMenu.add(createEventBtn);
+		createEventBtn.setMnemonic(KeyEvent.VK_N);	
+		JMenuItem editEventBtn = new JMenuItem("Edit an Existing Event");
+		editEventBtn.setMnemonic(KeyEvent.VK_E);
+		eventMenu.add(editEventBtn);
+		JMenuItem removeEventBtn = new JMenuItem("Delete an Existing Event");
+		removeEventBtn.setMnemonic(KeyEvent.VK_D);
+		eventMenu.add(removeEventBtn);
+
+		fileMenu.add(eventMenu);
+	}
+
+	private void buildContactMenu(JMenu fileMenu) {
+		//Build Contact Menu
+		JMenu contactMenu = new JMenu("Contacts");
+		contactMenu.setMnemonic(KeyEvent.VK_C);
+		JMenuItem createContactBtn = new JMenuItem("Create a New Contact");
+		createContactBtn.setMnemonic(KeyEvent.VK_N);
+		contactMenu.add(createContactBtn);
+		JMenuItem editContactBtn = new JMenuItem("Edit an Existing Contact");
+		editContactBtn.setMnemonic(KeyEvent.VK_E);
+		contactMenu.add(editContactBtn);
+		JMenuItem removeContactBtn = new JMenuItem("Remove an Existing Contact");
+		removeContactBtn.setMnemonic(KeyEvent.VK_D);
+		contactMenu.add(removeContactBtn);
+
+		fileMenu.add(contactMenu);
+	}
+
+	private void buildEditMenu() {
+		//Not Sure what to pute here for now now will keep it commented out
+		//this is commented out inthe build MenuBar section
+		JMenu editMenu = new JMenu("Edit");
+		editMenu.setMnemonic(KeyEvent.VK_E);
+		//Populate Edit Menu
+		menuBar.add(editMenu);
+	}
+
+	private void buildViewMenu() {
+		JMenu viewMenu = new JMenu("View");
+		viewMenu.setMnemonic(KeyEvent.VK_V);
+		//Populate View Menu
+		menuBar.add(viewMenu);
 	}
 }
