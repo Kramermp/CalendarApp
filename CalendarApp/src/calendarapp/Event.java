93b23edbeaae9cd171cd57c569e755f80afc0856
Michael Kramer <mpk5206@psu.edul>
2017-03-24 19:06:43 -0400

    Made event Serialiable and add fields

--- CalendarApp/src/calendarapp/Event.java
+++ CalendarApp/src/calendarapp/Event.java
@@ -5,10 +5,103 @@
  */
 package calendarapp;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author mpk5206
  */
-public class Event {
-    
+public class Event implements Serializable {
+    private String eventName = "";
+	private String eventTag = "";
+	private Date eventStartDate = new Date();
+	private Date eventEndDate = new Date();
+	private ArrayList<Contact> eventContactList = new ArrayList<Contact>();
+	private Location eventLocation;
+
+	/**
+	 * @return the eventName
+	 */
+	public String getEventName() {
+		return eventName;
+	}
+
+	/**
+	 * @param eventName the eventName to set
+	 */
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	/**
+	 * @return the eventTag
+	 */
+	public String getEventTag() {
+		return eventTag;
+	}
+
+	/**
+	 * @param eventTag the eventTag to set
+	 */
+	public void setEventTag(String eventTag) {
+		this.eventTag = eventTag;
+	}
+
+	/**
+	 * @return the eventStartDate
+	 */
+	public Date getEventStartDate() {
+		return eventStartDate;
+	}
+
+	/**
+	 * @param eventStartDate the eventStartDate to set
+	 */
+	public void setEventStartDate(Date eventStartDate) {
+		this.eventStartDate = eventStartDate;
+	}
+
+	/**
+	 * @return the eventEndDate
+	 */
+	public Date getEventEndDate() {
+		return eventEndDate;
+	}
+
+	/**
+	 * @param eventEndDate the eventEndDate to set
+	 */
+	public void setEventEndDate(Date eventEndDate) {
+		this.eventEndDate = eventEndDate;
+	}
+
+	/**
+	 * @return the eventContactList
+	 */
+	public ArrayList<Contact> getEventContactList() {
+		return eventContactList;
+	}
+
+	/**
+	 * @param eventContactList the eventContactList to set
+	 */
+	public void setEventContactList(ArrayList<Contact> eventContactList) {
+		this.eventContactList = eventContactList;
+	}
+
+	/**
+	 * @return the eventLocation
+	 */
+	public Location getEventLocation() {
+		return eventLocation;
+	}
+
+	/**
+	 * @param eventLocation the eventLocation to set
+	 */
+	public void setEventLocation(Location eventLocation) {
+		this.eventLocation = eventLocation;
+	}
 }
