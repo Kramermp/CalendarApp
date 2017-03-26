3a69beffa60cb78234dfc9943b99f2ef8be3d5ba
Eric Petrine <ericpetrine@gmail.com>
2017-03-26 19:39:37 -0400

    Started contact picker

--- CalendarApp/src/calendarapp/ui/ContactPicker.java
+++ CalendarApp/src/calendarapp/ui/ContactPicker.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calendarapp.ui;
 
 import calendarapp.Contact;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 /**
@@ -18,11 +18,23 @@ import javax.swing.JTextField;
  * @since .1
  */
 public class ContactPicker extends JPanel {
-	private ArrayList<Contact> selectedContacts;
-	
+		private ArrayList<Contact> selectedContacts = new ArrayList<Contact>();
+        private ArrayList<String> testContacts;
+        private ArrayList<String> testGroups;
+		private ArrayList<String> listOfContactNames;
+        private ArrayList<String> listOfGroups;
 	public ContactPicker() {
-		this.selectedContacts = new ArrayList<Contact>();
-		this.add(new JTextField("Contact Picker"));
+                this.testContacts = new ArrayList<String>();
+                testContacts.add("Alex");
+                testContacts.add("Ben");
+                testContacts.add("Connor");
+                testContacts.add("Jim");
+                testContacts.add("Patrick");
+                this.testGroups = new ArrayList<String>();
+                testGroups.add("IST 311");
+                testGroups.add("IST 301");
+                testGroups.add("IST 331");
+                addComponents();
 	}
 	
 	public ContactPicker(ArrayList<Contact> contacts) {
@@ -32,5 +44,92 @@ public class ContactPicker extends JPanel {
 	public ArrayList<Contact> getContacts() {
 		return this.selectedContacts;
 	}
-	
+	private void addComponents(){
+            this.setLayout(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+            JLabel groups = new JLabel("Groups:");
+            c.gridx = 0;
+            c.gridwidth = 1;
+            c.anchor = GridBagConstraints.EAST;
+            c.insets = new Insets(10,10,10,10);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridy = 0;
+            c.weightx = .75;
+            c.weighty = .1;
+            this.add(groups, c);
+            
+            c = new GridBagConstraints();
+            JLabel individuals = new JLabel("Individuals:");
+            c.gridx = 1;
+            c.gridwidth = 2;
+            c.anchor = GridBagConstraints.EAST;
+            c.insets = new Insets(10,10,10,10);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridy = 0;
+            c.weightx = .75;
+            c.weighty = .1;
+            this.add(individuals, c);
+            
+            for(int i = 0; i < testContacts.size(); i++){
+                c = new GridBagConstraints();
+                JRadioButton contactToAdd = new JRadioButton(testContacts.get(i));
+                if (contactToAdd.isSelected()){
+                    listOfContactNames.add(contactToAdd.getText());
+                }
+                c.gridx = 1;
+                c.gridwidth = 2;
+                c.anchor = GridBagConstraints.EAST;
+                c.insets = new Insets(10,10,10,10);
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridy = i + 1;
+                c.weightx = .75;
+                c.weighty = .1;
+                this.add(contactToAdd, c);
+            }
+            
+            for(int i = 0; i < testGroups.size(); i++){
+                c = new GridBagConstraints();
+                JRadioButton groupToAdd = new JRadioButton(testGroups.get(i));
+                if (groupToAdd.isSelected()){
+                    listOfGroups.add(groupToAdd.getText());
+                }
+                c.gridx = 0;
+                c.gridwidth = 2;
+                c.anchor = GridBagConstraints.EAST;
+                c.insets = new Insets(10,10,10,10);
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridy = i + 1;
+                c.weightx = .75;
+                c.weighty = .1;
+                this.add(groupToAdd, c);
+            }
+            
+        }
+        
 }
+
+
+
+
+
+
+//////////////////////////////////////////////
+//~201, EventUI
+//
+//		JLabel contactsLbl = new JLabel("Contacts:");
+//		c = new GridBagConstraints();
+//		c.anchor = GridBagConstraints.EAST;
+//		c.gridx = 3;
+//		c.gridy = 3;
+//		this.add(contactsLbl, c);
+//		
+//		contactArea = new ContactPicker();
+//		configureContactArea(contactArea);
+//		c = new GridBagConstraints();
+//		c.gridx = 4;
+//		c.weightx = .5;
+//		c.gridy = 3;
+//		c.fill = GridBagConstraints.BOTH;
+//                JScrollPane contacts = new JScrollPane (contactArea);
+//		this.add(contacts, c);
+
